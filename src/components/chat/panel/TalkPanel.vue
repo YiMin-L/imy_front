0ecbc909b65cfd096e49ca67eea0e9a1df54b955
yimin.liu <1357762370@qq.com>
2025-03-18 23:42:11 +0800

    对话框那显示人名

--- src/components/chat/panel/TalkPanel.vue
+++ src/components/chat/panel/TalkPanel.vue
@@ -71,6 +71,9 @@
                     :src="item.avatar"
                     @click.native="catFriendDetail(item.user_id)"
                   />
+                  <span class="avatar-name">
+                    {{ item.friend_remarks || item.nickname }}
+                  </span>
                 </aside>
                 <main class="main-column">
                   <div class="talk-title">
@@ -967,6 +970,16 @@ export default {
         }
       }
     }
+    .avatar-name {
+        font-size: 12px;
+        color: #606266;
+        margin-top: 5px;
+        width: 60px;
+        text-align: center;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
 
     .avatar-column {
       width: 40px;
